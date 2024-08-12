bfe673bc90eb8d34df47fe105208d7cb2ed6bdb8
[mrugank raut] <[raut21bcs39@iiitkottayam.ac.in]>
2024-08-13 00:18:45 +0530

    first commit

--- src/data/user.js
+++ src/data/user.js
@@ -1,28 +1,26 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
+		title: "Reactfolio by Mrugank",
 		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		email: "mmraut164@gmail.com",
 		logo: "../logo.png",
 	},
 
 	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		twitter: "https://x.com/MrugankR",
+		github: "https://github.com/Mrugank16",
+		linkedin: "https://www.linkedin.com/in/mrugank-raut-7928001b1/",
+		instagram: "https://instagram.com/"
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Full-stack web developer, and amateur Footballer.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a backend developer with expertise in Spring Boot & Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Mrugank Raut. I live in India, where I help what's coming next.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
@@ -35,49 +33,43 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "EcoMart",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+				"Developed a e-commerce platform using Spring Boot, featuring a RESTful API for managing products.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/java/java.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/Mrugank16/EcoMart",
 		},
 
 		{
-			title: "Project 2",
+			title: "Location Tracker Web App",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+				"Developed a real-time location tracking application using Node.js, Express, and Socket.io, with Leaflet.js for interactive map visualization.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/Mrugank16/Tracker_app",
+		},
+		{
+			title: "NeuroVision",
+			description:
+				"Developed a brain image classification web application utilizing Flask framework for backend, Keras for deep learning model integration, and React for frontend.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/Mrugank16/NeuroVision",
 		},
 
 		{
-			title: "Project 3",
+			title: "Translator Web App",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+				"Developed a multilingual text translation application using React, incorporating Microsoft’s Translator API for real-time language translation.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/Mrugank16/Translator-App",
 		},
 
-		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+		
 
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+		
 	],
 };
 
